0b902fa081eb5149dbbda7bb633691634afb3c1b
Viaasyafrina16 <syafrinavia@gmail.com>
2024-03-28 16:21:25 +0700

    system

--- praktikum3_algoritma/praktikum3_algoritma.cpp
+++ praktikum3_algoritma/praktikum3_algoritma.cpp
@@ -109,6 +109,13 @@ int main()
         q_short(0, n - 1);
         display();
         cout << "\n\nDo you want to continue? (y/n): ";
+
+        cin >> ch;
+        if (ch == 'n' || ch == 'N')
+            break;
+
+        system("pause");
+        system("cls");
         
 
       
