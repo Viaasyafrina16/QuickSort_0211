c85b0a749e8948b2a09b8dc0f171e82f0b89e57f
Viaasyafrina16 <syafrinavia@gmail.com>
2024-03-28 16:18:45 +0700

    povit

--- praktikum3_algoritma/praktikum3_algoritma.cpp
+++ praktikum3_algoritma/praktikum3_algoritma.cpp
@@ -48,6 +48,10 @@ void q_short(int low, int high)
     if (low > high) { //step 1
         return;
     }
+    pivot = arr[low]; //step2
+    i = low + 1; //step3
+    j = high; //step 4
+
         
 
       
