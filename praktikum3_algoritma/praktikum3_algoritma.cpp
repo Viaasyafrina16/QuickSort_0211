fee35036b82b6b67c7deed5cb004fa4bc8f4a3c3
Viaasyafrina16 <syafrinavia@gmail.com>
2024-03-28 16:19:58 +0700

    search for an element less than or equal to pivot

--- praktikum3_algoritma/praktikum3_algoritma.cpp
+++ praktikum3_algoritma/praktikum3_algoritma.cpp
@@ -61,6 +61,20 @@ void q_short(int low, int high)
             cmp_count++;
         }
         cmp_count++;
+        //search for an element less than or equal to pivot
+        while ((arr[j] > pivot) && (j >= low)) //step7
+        {
+            j--; //step8
+            cmp_count++;
+        }
+        cmp_count++;
+        if (i < j) //step 9
+        {
+            //swap the element at indexi whit the element at index j
+            swap(i, j);
+        }
+    }
+
         
 
       
